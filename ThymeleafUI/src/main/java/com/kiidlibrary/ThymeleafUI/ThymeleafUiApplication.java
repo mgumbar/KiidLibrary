e856e133811f38f491016cb5af81859d77c74b9a
mgumbar <mgumbar@gmail.com>
2019-12-01 11:13:14 +0100

    MERGE FOLDER

---
+++ ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/ThymeleafUiApplication.java
@@ -0,0 +1,13 @@
+package com.kiidlibrary.ThymeleafUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafUiApplication.class, args);
+	}
+
+}
---
+++ ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/controller/UIController.java
@@ -0,0 +1,17 @@
+package com.kiidlibrary.ThymeleafUI.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+//@RequestMapping(value = "/UI")
+public class UIController {
+
+    @RequestMapping("/")
+    public String accueil(Model model){
+
+        return "Accueil";
+    }
+}

a4c32af9de898b37de487033527cf7a3db5257be
mgumbar <mgumbar@gmail.com>
2019-12-05 23:20:07 +0100

    ZUUL

--- ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/ThymeleafUiApplication.java
+++ ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/ThymeleafUiApplication.java
@@ -2,10 +2,13 @@ package com.kiidlibrary.ThymeleafUI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.kiidlibrary.ThymeleafUI.proxies") // NOM DU PACKAGE PROXY
+@EnableDiscoveryClient
 public class ThymeleafUiApplication {
 
 	public static void main(String[] args) {
--- ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/proxies/MicroservicesKiid.java
+++ ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/proxies/MicroservicesKiid.java
@@ -1,7 +1,9 @@
 package com.kiidlibrary.ThymeleafUI.proxies;
 
 import com.kiidlibrary.ThymeleafUI.beans.KiidBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,12 +12,15 @@ import java.util.List;
 import java.util.Optional;
 
 //NOM DU MICRO SERVICE A APPELLER A RENSEIGNER DANS APPLICATION PROPERTIES DE kiidlibrary
-@FeignClient(name = "kiidlibrary", url = "192.168.1.16:9090")
+//@FeignClient(name = "kiidlibrary", url = "192.168.1.16:9090")
+@FeignClient(name = "zuul")
+@RibbonClient(name = "kiidlibrary")
 public interface MicroservicesKiid {
 
-    @RequestMapping(value = "/kiid", method = RequestMethod.GET)
+    @RequestMapping(value = "/kiidlibrary/kiid", method = RequestMethod.GET)
     public List<KiidBean> getAllKiids();
 
+    //@GetMapping( value = "/{kiidId}")
     @RequestMapping(value = "/{kiidId}", method = RequestMethod.GET)
     public KiidBean getKiid(@PathVariable("id") String kiidId);
 }

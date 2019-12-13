317265c6dd5048b25ea82511dc755b186acbd3b9
mgumbar <mgumbar@gmail.com>
2019-12-13 21:27:20 +0100

    ZIPPKIN

--- ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/controller/UIController.java
+++ ThymeleafUI/src/main/java/com/kiidlibrary/ThymeleafUI/controller/UIController.java
@@ -2,6 +2,8 @@ package com.kiidlibrary.ThymeleafUI.controller;
 
 import com.kiidlibrary.ThymeleafUI.beans.KiidBean;
 import com.kiidlibrary.ThymeleafUI.proxies.MicroservicesKiid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +16,13 @@ import java.util.List;
 //@RequestMapping(value = "/UI")
 public class UIController {
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     MicroservicesKiid microservicesKiid;
     @RequestMapping("/")
     public String accueil(Model model){
-
+        log.info("TRYING to get all kiids");
         List<KiidBean> kiids = microservicesKiid.getAllKiids();
         model.addAttribute("kiids", kiids);
         return "Accueil";

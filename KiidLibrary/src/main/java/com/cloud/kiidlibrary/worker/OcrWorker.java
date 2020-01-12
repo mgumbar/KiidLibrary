e978440be7c6cc2cd618941322f91b7cc3e6887f
mgumbar <mgumbar@gmail.com>
2020-01-12 04:17:05 +0100

    update path for LINUX LAST

--- KiidLibrary/src/main/java/com/cloud/kiidlibrary/worker/OcrWorker.java
+++ KiidLibrary/src/main/java/com/cloud/kiidlibrary/worker/OcrWorker.java
@@ -95,7 +95,8 @@ public class OcrWorker {
                 if (System.getProperty("os.name").toLowerCase().contains("windows"))
                     filePath = filePath.substring(1);
                 else if (filePath.toLowerCase().startsWith("file:"))
-                    filePath = "/classes/tessdata/";
+                    filePath = "BOOT-INF/classes/tessdata";
+
                 tesseract.setDatapath(filePath);
                 tesseract.setLanguage("eng");
                 String text = tesseract.doOCR(file);
